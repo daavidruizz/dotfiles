728767a80036e65bd6cf7db4c190f50c2b0c534c
David Ruiz <daavidruiz01@outlook.com>
2026-03-27 18:47:08 +0100

    Fixes waybar + nvidia fix

--- waybar/.config/waybar/gpu_temp.sh
+++ waybar/.config/waybar/gpu_temp.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Contenido del script
-nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits 2>/dev/null || echo "0"
+command -v nvidia-smi &>/dev/null || exit 0
+temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits 2>/dev/null)
+[ -n "$temp" ] && echo "$temp"
