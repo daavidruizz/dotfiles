19dd26211096489febe0cd6ba0a39b5714261d22
David Ruiz <daavidruiz01@outlook.com>
2026-05-17 18:58:23 +0200

    themes

--- hypr/.config/hypr/conf/env.lua
+++ hypr/.config/hypr/conf/env.lua
@@ -3,7 +3,8 @@
 ----------------------------------
 
 -- Cursor / display
-hl.env("XCURSOR_SIZE",  "24")
+hl.env("XCURSOR_THEME",   "Bibata-Modern-Ice")
+hl.env("XCURSOR_SIZE",    "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
 -- NVIDIA
