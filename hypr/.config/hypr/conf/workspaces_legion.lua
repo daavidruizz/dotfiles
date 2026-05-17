6de7a4830f0db5b9050d58e210ccefd6aa0a8ef6
David Ruiz <daavidruiz01@outlook.com>
2026-05-17 18:25:06 +0200

    Ajustes legion

--- hypr/.config/hypr/conf/workspaces_legion.lua
+++ hypr/.config/hypr/conf/workspaces_legion.lua
@@ -6,8 +6,8 @@
 --hl.exec_once("hypridle -c ~/.config/hypr/hypridle_legion.conf")
 
 -- SETUP LEGION — ajusta según monitores conectados
---hl.workspace_rule({ workspace = "1", monitor = "eDP-1", default = true, persistent = true })
---hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true })
---hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true })
---hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true })
---hl.workspace_rule({ workspace = "5", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1", persistent = true })
