f48ddf674d7cdfdc7152f012ea9ff5a42512316e
David Ruiz <daavidruiz01@outlook.com>
2026-05-24 14:22:16 +0200

    Iconos waybar + idle

--- hypr/.config/hypr/conf/monitors_msi.lua
+++ hypr/.config/hypr/conf/monitors_msi.lua
@@ -6,20 +6,23 @@
 -- DEFAULT: descomentar en PC nuevo si no se conoce la config exacta
 
 hl.monitor({
-    output      = "DP-2",
-    mode        = "2560x1440@180.00Hz",
-    position    = "0x0",
-    scale       = 1,
+	output = "DP-2",
+	mode = "2560x1440@180.00Hz",
+	position = "0x0",
+	scale = 1,
 })
 
 hl.monitor({
-    output      = "DP-1",
-    mode        = "2560x1440@180.00Hz",
-    position    = "2561x0",
-    scale       = 1,
+	output = "DP-1",
+	mode = "2560x1440@180.00Hz",
+	position = "2561x0",
+	scale = 1,
 })
 
 hl.monitor({
-    output      = "HDMI-A-2",
-    disabled    = true,
+	output = "HDMI-A-2",
+	mode = "1920x1080@60",
+	position = "960x-1080",
+	scale = 1,
+	disabled = true,
 })
